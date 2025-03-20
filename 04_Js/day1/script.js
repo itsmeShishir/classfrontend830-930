1ac7eb75ba3de831d2d0eed9535f32eba520478f
itsmeShishir <91515501+itsmeShishir@users.noreply.github.com>
2025-03-20 08:33:41 +0545

    js

---
+++ 04_Js/day1/script.js
@@ -0,0 +1,38 @@
+console.log("Outside file");
+
+// String
+let names  = "Hello world";
+console.log(names);
+
+//  String Concatenation
+let firstName = "John";
+let lastName = "hari";
+
+console.log(firstName + " "+ lastName);
+
+//  Template Literal
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+//  Strings Methods
+let text = "Hello world";
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.slice(0, 5));
+console.log(text.substring(0, 5));
+console.log(text.replace("world", "hari"));
+console.log(text.split(" "));
+console.log(text.trim());
+console.log(text.charAt(0));
+console.log(text.charCodeAt(0));
+console.log(text.indexOf("world"));
+console.log(text.lastIndexOf("world"));
+console.log(text.includes("world"));
+console.log(text.startsWith("Hello"));
+console.log(text.endsWith("world"));
+console.log(text.padStart(10, "0"));
+console.log(text.padEnd(10, "0"));
+
+
+
